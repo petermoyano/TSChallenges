8adf1f5b52f11d8dc3bb3b04e9f65dcadd1138af
Peter M <pmoyano@solvd.com>
2022-12-28 16:02:53 -0300

    solved

---
+++ challenge1/houses.ts
@@ -0,0 +1,6 @@
+const houses = [
+  { name: "Atreides", planets: "Calladan" },
+  { name: "Corrino", planets: ["Kaitan", "Salusa Secundus"] },
+  { name: "Harkonnen", planets: ["Giedi Prime", "Arrakis"] },
+];
+export default houses;
